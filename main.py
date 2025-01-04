a0d2575b98be603de5f31bebbd253a5d78d38e19
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-01-03 22:04:05 -0500

    [add] boiler plate code

---
+++ main.py
@@ -0,0 +1,33 @@
+from decouple import config
+
+from discord import Intents, Client, app_commands
+from discord.ext import commands, tasks    # pip install discord-ext-bot
+import discord   # pip install discord
+
+BOT_KEY = config('BOT_KEY')
+
+# Bot Constants
+intents = Intents.default()
+intents.message_content = True
+intents.reactions = True
+bot = commands.Bot(command_prefix="!", intents=intents, help_command=None) # Initialize bot
+
+# ========================================
+# ! COMMANDS
+# ========================================
+# Simple example command
+@bot.command(name='hello') # Activate using !hello
+async def hello_command(ctx):
+  await ctx.send(f'Hello, {ctx.author.name}!')
+
+
+# ========================================
+# RUN BOT
+# ========================================
+# Start bot
+@bot.event
+async def on_ready():
+  await bot.tree.sync()
+  print(f"{bot.user} is now running!")
+
+bot.run(token=BOT_KEY)
